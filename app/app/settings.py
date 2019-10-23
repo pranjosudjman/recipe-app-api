2661588f32071ac2c848f3afa4b3f70558315f81
Pranjo Sudjman <dicroatiano@gmail.com>
2019-10-23 19:22:59 +0200

    Added recepies and started with indredients

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
+    'recipe',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
